dc4f08008f5b9384e2ffb058ba956db78e79662c
bludotbat <bluiscool1337@gmail.com>
2021-10-07 19:16:56 -0500

    feat: StickyBeds

--- src/main/java/bluiscool/stickybed/StickyBed.java
+++ src/main/java/bluiscool/stickybed/StickyBed.java
@@ -7,16 +7,13 @@ import org.bukkit.block.data.type.Bed;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.inventory.InventoryInteractEvent;
-import org.bukkit.event.player.PlayerBedEnterEvent;
 import org.bukkit.event.player.PlayerBedLeaveEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.metadata.FixedMetadataValue;
-import org.bukkit.metadata.MetadataValue;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
 
 class StickyBedEvents implements Listener {
     StickyBed stickyBed;
@@ -27,16 +24,53 @@ class StickyBedEvents implements Listener {
 
     @EventHandler
     public void tryLeaveBed(PlayerBedLeaveEvent event) {
-        stickyBed.log("Test!");
+
         Block bed = event.getBed();
         if(bed.hasMetadata("sticky"))
         {
-            stickyBed.log("sticky!!!");
-    
+            event.setCancelled(true);
+
+            if(bed.getMetadata("sticky").get(0).asInt() != 1) return;
+
+            Bed bedAsBed = (Bed)bed.getBlockData();
+
+            Block otherBed = bed.getRelative(bedAsBed.getFacing().getOppositeFace());
+            if(!(otherBed.getBlockData() instanceof org.bukkit.block.data.type.Bed))
+                otherBed = bed.getRelative(bedAsBed.getFacing());
+
+            bed.setMetadata("sticky", new FixedMetadataValue(stickyBed, 2));
+            otherBed.setMetadata("sticky", new FixedMetadataValue(stickyBed, 2));
+
+            Block finalOtherBed = otherBed;
+            new BukkitRunnable() {
+                int ticks = 0;
+                @Override
+                public void run() {
+                    ticks++;
+
+                    if(ticks == 6) {
+                        event.getPlayer().teleport(bed.getLocation());
+                        event.getPlayer().damage(5);
+
+                        bed.getWorld().dropItemNaturally(bed.getLocation(), new ItemStack(bed.getType(), 1));
+                        bed.setType(Material.AIR);
+                        bed.removeMetadata("sticky", stickyBed);
+                        finalOtherBed.removeMetadata("sticky", stickyBed);
+                        this.cancel();
+                        return;
+                    }
+
+                    bed.getWorld().playSound(bed.getLocation(), Sound.ENTITY_PLAYER_HURT, 5 + ticks, 1.0f);
+                    bed.getWorld().playSound(bed.getLocation(), Sound.BLOCK_SLIME_BLOCK_PLACE, 5 + ticks, 1.0f);
+                }
+
+            }.runTaskTimer(stickyBed, 0, 20L);
         }
     }
 
+    @EventHandler
     public void onBedClick(PlayerInteractEvent event) {
+        if(event.getClickedBlock() == null) return;
         if(!(event.getClickedBlock().getBlockData() instanceof org.bukkit.block.data.type.Bed))
             return;
 
@@ -45,11 +79,21 @@ class StickyBedEvents implements Listener {
         ItemStack handItem = inventory.getItemInMainHand();
 
         if(handItem.getType() == Material.SLIME_BALL) {
+            event.setCancelled(true);
+
             Block bed = event.getClickedBlock();
             if(!bed.hasMetadata("sticky")) {
                 event.setCancelled(true);
-                bed.setMetadata("sticky", new FixedMetadataValue(stickyBed, true));
-                bed.setType(Material.GREEN_BED);
+
+                Bed bedAsBed = (Bed)bed.getBlockData();
+
+                Block otherBed = bed.getRelative(bedAsBed.getFacing().getOppositeFace());
+                if(!(otherBed.getBlockData() instanceof org.bukkit.block.data.type.Bed))
+                    otherBed = bed.getRelative(bedAsBed.getFacing());
+
+                bed.setMetadata("sticky", new FixedMetadataValue(stickyBed, 1));
+                otherBed.setMetadata("sticky", new FixedMetadataValue(stickyBed, 1));
+
                 handItem.setAmount(handItem.getAmount() - 1);
                 bed.getWorld().playSound(bed.getLocation(), Sound.BLOCK_SLIME_BLOCK_PLACE, 5, 1.0f);
             }
@@ -58,14 +102,9 @@ class StickyBedEvents implements Listener {
 }
 
 public final class StickyBed extends JavaPlugin {
-    public void log(String msg) {
-        this.getLogger().info(msg);
-    }
-
     @Override
     public void onEnable() {
         getServer().getPluginManager().registerEvents(new StickyBedEvents(this), this);
-        log("Ready!");
     }
 
     @Override
